e3f52da825fc4073ca96d8a336a489b6fe026aca
Sneha2120 <snehagowdas473@gmail.com>
2024-11-25 14:04:46 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,14 @@
+package com.anpc8232.day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+		int a = 20;
+		int b = 30;
+		int c = 40;
+		int d = 20;
+		int e = 40;
+	}
+
+}
